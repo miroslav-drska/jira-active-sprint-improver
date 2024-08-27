fb66ca95d95a0703b2f79f994851e93dd12bbbbb
Miroslav Drska <miroslav.drska@ceskatelevize.cz>
2024-08-27 21:34:37 +0200

    Init

---
+++ content_script.js
@@ -0,0 +1,21 @@
+console.log('JIRA Active Sprint Improver is active.');
+
+// Add css style block to html
+const style = document.createElement('style');
+style.innerHTML = `
+  .ghx-swimlane .ghx-heading-expander {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-left: 24px;
+    padding-right: 24px;
+    margin-top: -10px;
+    margin-bottom: -10px;
+    margin-right: 16px;
+  }
+  .ghx-swimlane .ghx-heading-expander svg {
+    position: absolute;
+    top: 10px;
+    left: 16px;
+  }
+`;
+document.head.appendChild(style);
