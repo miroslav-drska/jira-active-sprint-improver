c05ff8ac35b68ff2556e9597b1e5b98fc40df552
Miroslav Drska <miroslav.drska@ceskatelevize.cz>
2024-08-29 08:35:42 +0200

    Limit to JIRA pages

--- src/content_script.js
+++ src/content_script.js
@@ -1,21 +1,23 @@
 console.log('JIRA Active Sprint Improver is active.');
 
-// Add css style block to html
-const style = document.createElement('style');
-style.innerHTML = `
-  .ghx-swimlane .ghx-heading-expander {
-    padding-top: 16px;
-    padding-bottom: 16px;
-    padding-left: 24px;
-    padding-right: 24px;
-    margin-top: -10px;
-    margin-bottom: -10px;
-    margin-right: 16px;
-  }
-  .ghx-swimlane .ghx-heading-expander svg {
-    position: absolute;
-    top: 10px;
-    left: 16px;
-  }
-`;
-document.head.appendChild(style);
+// Run only on JIRA pages
+if (window.location.href.includes('jira')) {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    .ghx-swimlane .ghx-heading-expander {
+      padding-top: 16px;
+      padding-bottom: 16px;
+      padding-left: 24px;
+      padding-right: 24px;
+      margin-top: -10px;
+      margin-bottom: -10px;
+      margin-right: 16px;
+    }
+    .ghx-swimlane .ghx-heading-expander svg {
+      position: absolute;
+      top: 10px;
+      left: 16px;
+    }
+  `;
+  document.head.appendChild(style);
+}
